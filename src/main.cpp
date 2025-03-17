2fc6b2175ed0c827020210d65a1d158c99417726
Ivan Golubev <ivan.x64@protonmail.com>
2025-03-17 22:15:46 +0000

    Multithreaded ping pong

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,56 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <condition_variable>
+#include <stop_token>
+#include <format>
+#include <syncstream>
+#include <chrono>
 
-struct MyStruct {
-	char a;
-	int b;
+using namespace std::literals::chrono_literals;
+
+struct Message {
+	int counter;
+	std::string message;
 };
 
-void modifyStruct(MyStruct& s) {
-	s.b = 42;
+void runPingPong(std::mutex& mtx, std::condition_variable& cv, std::queue<Message>& msg_queue, std::stop_source& stop_src) {
+	std::stop_token stop_tkn = stop_src.get_token();
+	std::thread::id this_id = std::this_thread::get_id();
+	std::unique_lock lck(mtx, std::defer_lock);
+
+	while (!stop_tkn.stop_requested()) {
+		lck.lock();
+		cv.wait(lck, [&msg_queue]{ return !msg_queue.empty(); });
+		Message incoming_msg = msg_queue.front();
+		msg_queue.pop();
+		std::osyncstream(std::cout) << std::format("T[{}] received [c={}, t={}]", this_id, incoming_msg.counter, incoming_msg.message) << "\n";
+		int counter = incoming_msg.counter + 1;
+		if (counter <= 100)
+			msg_queue.emplace(counter, std::format("Hello from T[{}]", this_id));
+		else
+			stop_src.request_stop(); // final message sent - close the communication channels
+		lck.unlock();
+		cv.notify_one();
+		std::this_thread::sleep_for(10ms);
+	}
 }
 
 int main() {
-	int x = 42;
-	MyStruct str1;
-	std::cout << "Hello world!\n"; 
-	std::cout << "Hello " << x << "\n";
-	
-	str1.a = 1;
-	str1.b = 2;
-	modifyStruct(str1);
-	std::cout << "Hello struct {a=" << str1.a << ", b=" << str1.b << "}\n";
+	std::mutex mtx;
+	std::condition_variable cv;
+	std::queue<Message> msg_queue;
+	std::stop_source stop_src;
+
+	msg_queue.emplace(0, "first message");
+
+	{
+		std::jthread t1(runPingPong, std::ref(mtx), std::ref(cv), std::ref(msg_queue), std::ref(stop_src));
+		std::jthread t2(runPingPong, std::ref(mtx), std::ref(cv), std::ref(msg_queue), std::ref(stop_src));
+	}
+
+	std::osyncstream(std::cout) << "Finished execution\n";
 	return 0;
 }
